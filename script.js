4471e78db96e0d5de6d39881992d6e93854efb5d
Tahina93 <anne.tahina.metz@gmail.com>
2019-05-20 11:19:24 +0200

    n2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ********* Fonctionnalité 1 **********
 function footerCounter() {
     console.log(`Number of times the footer has been clicked : ${footerClicks++}`)
 }
@@ -5,3 +6,15 @@ function footerCounter() {
 let footerClicks = 1
 
 document.querySelector("footer").addEventListener("click", footerCounter)
+
+// ********* Fonctionnalité 2 **********
+const hamburgerBtn = document.querySelector("button.navbar-toggler");
+
+
+function toggleCollapse() {
+  const navBar = document.getElementById("navbarHeader");
+  
+  navBar.classList.toggle("collapse")
+}
+
+hamburgerBtn.addEventListener("click", toggleCollapse)
